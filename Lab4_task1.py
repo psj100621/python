eb31386bdff77c2d0c576698c6c60d426f4a9333
psj100621 <psjjune100621@gmail.com>
2023-06-03 13:22:33 +0900

    Finished Lab4_task1

--- Lab4_task1.py
+++ Lab4_task1.py
@@ -25,26 +25,52 @@ def quotient(x,y):
 def remainder(x,y):
     return (x+y) % 10
 
+def sum(x,y):
+    return (x+y)
+
 def drop_beeper_N_times(X):
     for i in range(X):
         hubo.drop_beeper()
-oneline()
-
-x = pick_all_beeper()
-hubo.turn_left()
-hubo.move()
-y = pick_all_beeper()
-r = remainder(x,y)
-q = quotient(x,y)
-for i in range(2):
+def addition():
+    x = pick_all_beeper()
     hubo.turn_left()
-hubo.move()
-drop_beeper_N_times(r)
-for i in range(2):
-    hubo.turn_left()
-for i in range(2):
     hubo.move()
-hubo.turn_left()
-hubo.move()
-drop_beeper_N_times(q)
-hubo.move()
+    y = pick_all_beeper()
+    r = remainder(x,y)
+    q = quotient(x,y)
+    for i in range(2):
+        hubo.turn_left()
+    hubo.move()
+    drop_beeper_N_times(r)
+    for i in range(2):
+        hubo.turn_left()
+    for i in range(2):
+        hubo.move()
+    hubo.turn_left()
+    hubo.move()
+    drop_beeper_N_times(q)
+    for i in range(4):
+        hubo.move()
+        hubo.turn_left()
+    hubo.turn_left()
+    a = pick_all_beeper()
+    hubo.move()
+    b = pick_all_beeper()
+    c = sum(a,b)
+    hubo.move()
+    drop_beeper_N_times(c)
+    turnright()
+    hubo.move()
+    hubo.turn_left()
+    hubo.turn_left()
+    x = 0
+    y = 0
+    r = 0
+    q = 0
+    a = 0
+    b = 0
+    c = 0
+
+oneline()
+for i in range(2):
+    addition()
